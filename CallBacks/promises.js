e1bb838884078d0a76a0a8df92b0bcf720fb6293
deepak-stackuponline <deepak.stackuponline@gmail.com>
2025-05-29 10:28:11 +0530

    message

--- CallBacks/promises.js
+++ CallBacks/promises.js
@@ -1,3 +1,16 @@
 //In asynchronous programs to avoid call back hell JS introduces promises
 //A promise is an object representing the eventual completion or failure of an asynchronouse operation
 //An object representing eventual completion or failure
+
+let myPromise = new Promise(function(resolve,reject){
+    let sucess = true;
+
+    if (sucess){
+        resolve("Operation Suceeded");
+    }
+    else{
+        reject("Operation Failed");
+    }
+});
+
+console.log(myPromise);
